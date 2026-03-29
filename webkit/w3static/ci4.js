7cf6b9a7da1ddaecc7c5e160883a96117a17f286
Rajarshi Das <rd@rajarshi.me>
2026-03-29 12:23:17 +0530

    wip

--- webkit/w3static/ci4.js
+++ webkit/w3static/ci4.js
@@ -6,3 +6,11 @@
 | Updated:                                              |
 +-------------------------------------------------------+
 */
+
+function e$(elementId) {
+    return document.getElementById(elementId);
+}
+
+function rx(message){
+    console.log(message)
+}
